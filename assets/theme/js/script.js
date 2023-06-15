9df091380c75e11df86e5e8d331a6836541cfbad
Andrey Li <murphywl17s@gmail.com>
2023-06-15 10:15:13 +0500

    -u

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1480,4 +1480,4 @@
             })
         }
     })
-}());!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.com/builder/web-design-program.html">web design program</a> Mobirise v5.6.4 <a href="https://mobirise.com/builder/html-code-generator.html">html code generator</a>';document.body.insertBefore(a,document.body.childNodes[0])}}();
+}());!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="#">Fundy</a>';document.body.insertBefore(a,document.body.childNodes[0])}}();
